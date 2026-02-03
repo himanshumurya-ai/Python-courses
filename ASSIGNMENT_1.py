2054047347b46e2c923e25236a90b843a3b67f5d
himanshumurya-ai <himanshu.murya@gmail.com>
2026-02-03 18:28:53 +0530

    Add files via upload

---
+++ ASSIGNMENT_1.py
@@ -0,0 +1,21 @@
+# Task 1: Perform Basic Mathematical Operations
+
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+Addition = int(a) + int(b)
+Subtraction = int(a) - int(b)
+Multiplication = int(a) * int(b)
+Division = int(a) / int(b)
+print("Addition: " + str(Addition))
+print("Subtraction: " + str(Subtraction))
+print("Multiplication: " + str(Multiplication))
+print("Division: " + str(Division))
+
+
+# Task 2: Create a Personalized Greeting
+
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+print(f"Hello, {firstName} {lastName}! Welcome to the Python program")
